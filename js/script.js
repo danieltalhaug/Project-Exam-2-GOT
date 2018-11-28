d7fdfaf8b77c53703d73409a99320d2922b5a2bf
danieltalhaug <daniel.talhaug@outlook.com>
2018-11-28 14:42:02 +0100

    Button finished and function added to move overlay on click

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // JavaScript Document
+var characterOverlay = document.getElementById('characterSelection');
+
+function closeOverlay() {
+	// moves the overlay up by 4000pixels
+	characterOverlay.style.transform = "translateY(-4000px)";
+}
