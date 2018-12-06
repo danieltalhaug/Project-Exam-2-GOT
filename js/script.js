546d301d8a1e3626142443d39959d774c6d2ccd9
danieltalhaug <daniel.talhaug@outlook.com>
2018-12-06 13:45:02 +0100

    Made some changes to CSS. Some borders made gray and transform scale has been added to character selection to make selected characters look like they're pressed down

--- js/script.js
+++ js/script.js
@@ -25,6 +25,37 @@ var characterOverlay = document.getElementById('characterSelection'),
 	debugPlayer2Char = document.getElementById('player2char'),
 	debugPlayer2Pos = document.getElementById('player2pos');
 
+var tile1 = document.getElementById('tile1'),
+	tile2 = document.getElementById('tile2'),
+	tile3 = document.getElementById('tile3'),
+	tile4 = document.getElementById('tile4'),
+	tile5 = document.getElementById('tile5'),
+	tile6 = document.getElementById('tile6'),
+	tile7 = document.getElementById('tile7'),
+	tile8 = document.getElementById('tile8'),
+	tile9 = document.getElementById('tile9'),
+	tile10 = document.getElementById('tile10'),
+	tile11 = document.getElementById('tile11'),
+	tile12 = document.getElementById('tile12'),
+	tile13 = document.getElementById('tile13'),
+	tile14 = document.getElementById('tile14'),
+	tile15 = document.getElementById('tile15'),
+	tile16 = document.getElementById('tile16'),
+	tile17 = document.getElementById('tile17'),
+	tile18 = document.getElementById('tile18'),
+	tile19 = document.getElementById('tile19'),
+	tile20 = document.getElementById('tile20'),
+	tile21 = document.getElementById('tile21'),
+	tile22 = document.getElementById('tile22'),
+	tile23 = document.getElementById('tile23'),
+	tile24 = document.getElementById('tile24'),
+	tile25 = document.getElementById('tile25'),
+	tile26 = document.getElementById('tile26'),
+	tile27 = document.getElementById('tile27'),
+	tile28 = document.getElementById('tile28'),
+	tile29 = document.getElementById('tile29'),
+	tile30 = document.getElementById('tile30');
+
 // Feed debug box with information. This updates itself every 1 second. 
 function debugMonitor() {
 	setInterval(function(){
@@ -34,7 +65,6 @@ function debugMonitor() {
 		debugPlayer2Pos.innerHTML = "player 2 pos : " + player2Position;
 	}, 1000);
 }
-debugMonitor();
 
 // Closes the Character selection overlay
 function closeOverlay() {
@@ -57,18 +87,19 @@ function selectHeroText() {
 		characterSelectText.innerHTML = "Player 2, Please select your Hero."
 	}
 }
-characterSelectText.innerHTML = "Player 1, Please select your Hero."
 // Character select functions. 
 function selectRobbStark() {
 	if ((player1Character.length === 0) && (player2Character.length === 0)) {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Robb Stark";
 		robbStarkCard.style.filter = "grayscale(0)";
+		robbStarkCard.style.transform = "scale(.9)";
 		characterSelectText.innerHTML = "Player 2, Please select your Hero."
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Robb Stark";
 		robbStarkCard.style.filter = "grayscale(0)";
+		robbStarkCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -81,10 +112,12 @@ function selectSansaStark() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Sansa Stark";
 		sansaStarkCard.style.filter = "grayscale(0)";
+		sansaStarkCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Sansa Stark";
 		sansaStarkCard.style.filter = "grayscale(0)";
+		sansaStarkCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -97,10 +130,12 @@ function selectAryaStark() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Arya Stark";
 		aryaStarkCard.style.filter = "grayscale(0)";
+		aryaStarkCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Arya Stark";
 		aryaStarkCard.style.filter = "grayscale(0)";
+		aryaStarkCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -113,10 +148,12 @@ function selectJonSnow() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Jon Snow";
 		jonSnowCard.style.filter = "grayscale(0)";
+		jonSnowCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Jon Snow";
 		jonSnowCard.style.filter = "grayscale(0)";
+		jonSnowCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -129,10 +166,12 @@ function selectTheonGreyjoy() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Theon Greyjoy";
 		theonGreyjoyCard.style.filter = "grayscale(0)";
+		theonGreyjoyCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Theon Greyjoy";
 		theonGreyjoyCard.style.filter = "grayscale(0)";
+		theonGreyjoyCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -145,10 +184,12 @@ function selectTyrionLannister() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Tyrion Lannister";
 		tyrionLannisterCard.style.filter = "grayscale(0)";
+		tyrionLannisterCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Tyrion Lannister";
 		tyrionLannisterCard.style.filter = "grayscale(0)";
+		tyrionLannisterCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -161,10 +202,12 @@ function selectStannisBaratheon() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Stannis Baratheon";
 		stannisBaratheonCard.style.filter = "grayscale(0)";
+		stannisBaratheonCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Stannis Baratheon";
 		stannisBaratheonCard.style.filter = "grayscale(0)";
+		stannisBaratheonCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -177,10 +220,12 @@ function selectDavosSeaworth() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Davos Seaworth";
 		davosSeaworthCard.style.filter = "grayscale(0)";
+		davosSeaworthCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Davos Seaworth";
 		davosSeaworthCard.style.filter = "grayscale(0)";
+		davosSeaworthCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -193,10 +238,12 @@ function selectDaenerysTargaryen() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Daenerys Targaryen";
 		daenerysTargaryenCard.style.filter = "grayscale(0)";
+		daenerysTargaryenCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Daenerys Targaryen";
 		daenerysTargaryenCard.style.filter = "grayscale(0)";
+		daenerysTargaryenCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -209,10 +256,12 @@ function selectViserysTargaryen() {
 		// If player1character and player2character is zero set player 1 to character
 		player1Character = "Viserys Targaryen";
 		viserysTargaryenCard.style.filter = "grayscale(0)";
+		viserysTargaryenCard.style.transform = "scale(.9)";
 	} else if ((player1Character.length > 0) && (player2Character.length === 0)) {
 		// if player1character is greater than zero and player2character is zero set player 2 character
 		player2Character = "Viserys Targaryen";
 		viserysTargaryenCard.style.filter = "grayscale(0)";
+		viserysTargaryenCard.style.transform = "scale(.9)";
 		startGame();
 		closeOverlay();
 	} else if ((player1Character.length > 0) && (player2Character.length > 0)) {
@@ -220,3 +269,7 @@ function selectViserysTargaryen() {
 		return
 	}
 }
+
+// Call functions here
+selectHeroText();
+debugMonitor();
